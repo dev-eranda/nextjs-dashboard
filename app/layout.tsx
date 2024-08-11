9e0525b5eb7a3fc7564bc19f3ace449c045167a1
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-08-11 15:13:23 +0530

    Fix: Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,24 @@
-import '@/app/ui/global.css'
+import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Metadata } from 'next';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-     <body className={`${inter.className} antialiased`}>
-     <SpeedInsights/>
-     {children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <SpeedInsights />
+        {children}
+      </body>
     </html>
   );
 }
